1eeffc231b22dcac3133950f5ce53e99ccb396fe
Alexnander Nasokin <alexnas2python@gmail.com>
2023-01-12 19:12:25 +0300

    Create Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
 	<v-app>
+		<Navbar />
 	</v-app>
 </template>
 
 <script>
-
+import Navbar from '@/components/Navbar.vue';
 export default {
 	name: 'App',
+	components: {
+		Navbar
+	}
 };
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-container>
+		<v-app-bar color="orange accent-4" dark>
+			<v-spacer />
+			<v-btn outlined class="ml-3">Main</v-btn>
+			<v-btn outlined class="ml-3">Photos</v-btn>
+		</v-app-bar>
+	</v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
