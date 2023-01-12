85aecdcb0492ad26aeafb7bd78fe243dcc250f91
Alexnander Nasokin <alexnas2python@gmail.com>
2023-01-12 21:24:45 +0300

    Create component Photo and dinamic mapping of array of photos

---
+++ src/components/photo/Photo.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		{{ photo.id }}. {{ photo.title }}
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		photo: {
+			type: Object,
+			required: true
+		}
+	}
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,12 +1,23 @@
 <template>
-	<div>
-		<h1>Photos</h1>
-	</div>
+	<v-container>
+		<Photo v-for="photo in photos" :photo="photo" />
+	</v-container>
 </template>
 
 <script>
+import Photo from '@/components/photo/Photo.vue';
 export default {
-
+	components: {
+		Photo
+	},
+	data: () => ({
+		photos: [
+			{ id: 1, title: 'photo 1' },
+			{ id: 2, title: 'photo 2' },
+			{ id: 3, title: 'photo 3' },
+			{ id: 4, title: 'photo 4' },
+		]
+	})
 }
 </script>
 
